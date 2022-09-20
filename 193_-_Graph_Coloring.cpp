f76686812daacc465031e4f566ea578698e42507
Xarma123 <92310696+Xarma123@users.noreply.github.com>
2022-09-20 13:08:02 +0530

    f

--- 193_-_Graph_Coloring.cpp
+++ 193_-_Graph_Coloring.cpp
@@ -1,27 +1,99 @@
-#include <bits/stdc++.h>
-#define lol long long
+#include <cstdio>
+#include <vector>
+#include <bitset>
 using namespace std;
-map < lol, vector<lol> mp;
+
+typedef vector<int> vi;
+
+static int WHITE = 0;
+static int BLACK = 1;
+
+bitset<110> colored;
+int color[110];
+vector<vi> AdjList;
+int tc, n, k, ans;
+vi ansls;
+
+void solve(int u)
+{
+    if (u == n)
+    {
+        int b = 0, w = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (color[i] == BLACK)
+                b++;
+            else
+                w++;
+        }
+        if (b > ans)
+        {
+            ans = b;
+            ansls.clear();
+            for (int i = 0; i < n; i++)
+            {
+                if (color[i] == BLACK)
+                    ansls.push_back(i);
+            }
+        }
+        return;
+    }
+
+    bool canBlack = true;
+
+    // check allowed color
+    for (int i = 0; i < (int)AdjList[u].size(); i++)
+    {
+        int v = AdjList[u][i];
+        if (colored[v] && color[v] == BLACK)
+        {
+            canBlack = false;
+            break;
+        }
+    }
+
+    colored.set(u);
+    if (canBlack)
+    {
+        color[u] = BLACK;
+        solve(u + 1);
+    }
+    color[u] = WHITE;
+    solve(u + 1);
+    colored.reset(u);
+}
+
 int main()
 {
-    std::ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    lol t;
-    cin >> t;
-    while (t--)
+    int u, v;
+
+    scanf("%d", &tc);
+    while (tc--)
     {
-        lol n, k;
-        cin >> n >> k;
-        mp.clear();
-        while (k--)
+        scanf("%d %d", &n, &k);
+
+        AdjList.assign(n, vi());
+        colored.reset();
+        ans = -1;
+        for (int i = 0; i < k; i++)
         {
-            lol a, b;
-            cin >> a >> b;
-            mp[a].push_back(b);
-            mp[b].push_back(a);
-        } 
-        
+            scanf("%d %d", &u, &v);
+            u--;
+            v--;
+            AdjList[u].push_back(v);
+            AdjList[v].push_back(u);
+        }
+
+        solve(0);
+
+        printf("%d\n", ans);
+        for (int i = 0; i < (int)ansls.size(); i++)
+        {
+            printf("%d", ansls[i] + 1);
+            if (i < (int)ansls.size() - 1)
+                printf(" ");
+        }
+        printf("\n");
     }
 
     return 0;
