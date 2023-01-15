a1d4da35b0db199655e12e1a682abf2a643538f0
Xarma123 <92310696+Xarma123@users.noreply.github.com>
2023-01-15 22:14:53 +0530

    f

--- A_Coder.cpp
+++ A_Coder.cpp
@@ -6,6 +6,30 @@ int main()
     std::ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
+    lol n;
+    cin >> n;
+    cout << (n / 2) * 1ll * (n / 2) + ceill(n / (long double)2) * 1ll * ceill(n / (long double)2) << endl;
+    for (lol i = 0; i < n; i++)
+    {
+        for (lol j = 0; j < n; j++)
+        {
+            if ((i % 2 == 0))
+            {
+                if (j % 2 == 0)
+                    cout << 'C';
+                else
+                    cout << '.';
+            }
+            else
+            {
+                if (j % 2 == 0)
+                    cout << '.';
+                else
+                    cout << 'C';
+            }
+        }
+        cout << endl;
+    }
 
     return 0;
 }
