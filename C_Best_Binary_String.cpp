36b53d055197543b6b53a27df63e2d01657c8980
Xarma123 <92310696+Xarma123@users.noreply.github.com>
2023-05-31 09:01:00 +0530

    f

---
+++ C_Best_Binary_String.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define lol long long
+int main()
+{
+    lol t;
+    cin >> t;
+    while (t--)
+    {
+        string s;
+        cin >> s;
+        lol n = s.size();
+        
+    }
+}
